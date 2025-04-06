5e687d44ce61e64e736225285e79644072d808cd
Maël <119709626+maeldolium@users.noreply.github.com>
2025-04-06 11:23:27 +0200

    Squashed commit of the following:
    commit f245f45796417ad09cce4e896555e7ad29af22de
    Author: Maël <119709626+maeldolium@users.noreply.github.com>
    Date:   Sun Apr 6 11:22:10 2025 +0200
    
        Squashed commit of the following:
    
        commit f711aafc7136a0ad197f8974a6c0f5cd0699f8f0
        Author: Maël <119709626+maeldolium@users.noreply.github.com>
        Date:   Sun Apr 6 11:02:56 2025 +0200
    
            Add projects and styles
    
        commit b11bcde8b69a4a957959395ae533c8eb1d084738
        Author: Maël <119709626+maeldolium@users.noreply.github.com>
        Date:   Thu Apr 3 11:55:37 2025 +0200
    
            Add contact and footer
    
        commit e0795c783eb8a4035da53d097533ee7ca81c88cd
        Author: Maël <119709626+maeldolium@users.noreply.github.com>
        Date:   Thu Apr 3 09:17:43 2025 +0200
    
            Add projects component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
 // Import du module CSS
+import { useState } from 'react';  // Importer useState pour gérer l'état du menu burger
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);  // État pour gérer l'ouverture du menu
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);  // Inverse l'état du menu
+    };
+
     return (
         <nav className={styles.navbar}> 
-            <ul className={styles.navList}>
+            {/* Menu burger */}
+            <div className={styles.menuIcon} onClick={toggleMenu}>
+                <div className={styles.burger}></div>
+                <div className={styles.burger}></div>
+                <div className={styles.burger}></div>
+            </div>
+
+            {/* Liste des liens de navigation */}
+            <ul className={`${styles.navList} ${menuOpen ? styles.open : ''}`}>
                 <li><a href="#hero">Accueil</a></li>
                 <li><a href="#skills">Compétences</a></li>
                 <li><a href="#projects">Projets</a></li> 
